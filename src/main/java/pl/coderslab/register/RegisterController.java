72bb854c1f882fe0b321995114e24a86f2aa9754
kriskoder <kris.koder@o2.pl>
2019-05-13 10:10:42 +0200

    added registration controller

---
+++ src/main/java/pl/coderslab/register/RegisterController.java
@@ -0,0 +1,36 @@
+package pl.coderslab.register;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.user.User;
+import pl.coderslab.user.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    private UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String register(@Valid Model model) {
+        model.addAttribute("user", new User());
+        return "register/register";
+    }
+
+    @PostMapping("")
+    public String register(@Valid @ModelAttribute User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "register/register";
+        }
+        userService.create(user);
+        return "login/login";
+    }
+}
